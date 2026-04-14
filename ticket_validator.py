af3774f82cf3bfa6800e001306e2a041243c386f
Melo <jmmelo.edu@gmail.com>
2026-04-14 12:11:14 +0000

    Added Validate_ticket code in ticket_validator.py

--- ticket_validator.py
+++ ticket_validator.py
@@ -1,5 +1,18 @@
 def validate_ticket (code):
-    pass
+    if not isinstance(code, str):
+        raise TypeError ("Code must be a string")
+
+    if len(code) != 8:
+        return False
+
+    if not code.startswith("TK"):
+        return False
+
+    if not code [2: ].isdigit():
+        return False
+    
+    return True
+
 
 def get_ticket_tier (code):
     pass
